1ee9bf22c638ecb0ca6e6d4c5d0c9900507381bd
Bob Reselman <bob@CogArtTech.com>
2022-09-15 17:36:03 -0700

    putting in logging

--- simple-client/index.js
+++ simple-client/index.js
@@ -11,17 +11,23 @@ async function main(){
     const privateKey = process.env.PRIVATE_KEY;
 
     const node = {'127.0.0.1:50211': new AccountId(3)}
+    console.log("Creating client");
     const client = Client.forNetwork(node).setMirrorNetwork('127.0.0.1:5600');
+    console.log("Created client");
 
     client.setOperator(AccountId.fromString(accountId), PrivateKey.fromString(privateKey));
-
+    console.log("Creating account");
     const newAccount = await new AccountCreateTransaction()
         .setKey(PrivateKey.fromString(privateKey))
         .setInitialBalance(new Hbar(1))
         .execute(client);
+    console.log("Created account");
 
+    console.log("Getting receipt");
     const receipt = await newAccount.getReceipt(client);
+    console.log("Got receipt");
     const newAccountId = receipt.accountId;
+
     console.log(JSON.stringify({newAccountId}))
 }
 
